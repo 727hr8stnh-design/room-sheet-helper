fa38123b191d3005ade74eb583dc22d5dbfdb802
727hr8stnh-design <727hr8stnh@privaterelay.appleid.com>
2026-09-08 09:44:17 +0900

    Fix PWA cache so latest room helper is served

--- sw.js
+++ sw.js
@@ -1,1 +1,42 @@
-const CACHE='room-helper-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='room-helper-v3';
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(['./manifest.webmanifest']))
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.mode==='navigate' || request.destination==='document'){
+    event.respondWith(
+      fetch(request,{cache:'no-store'})
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(request).then(r=>r||caches.match('./')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(request,copy));
+        return response;
+      })
+      .catch(()=>caches.match(request))
+  );
+});
